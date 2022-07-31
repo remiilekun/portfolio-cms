c51af8f3f20565dbb76cc67c76a1c8711a9bbfc9
Remilekun Salami <remilekun.salami@gmail.com>
2022-07-31 23:15:41 +0100

    feat: netlify and cloudinary plugin

--- config/plugins.js
+++ config/plugins.js
@@ -11,4 +11,27 @@ module.exports = {
       },
     },
   },
+  "netlify-deployments": {
+    enabled: true,
+    config: {
+      buildHook: process.env.NETLIFY_BUILD_HOOK,
+      accessToken: process.env.NETLIFY_ACCESS_TOKEN,
+      siteId: process.env.NETLIFY_SITE_ID,
+    },
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key: process.env.CLOUDINARY_KEY,
+        api_secret: process.env.CLOUDINARY_SECRET,
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
 };
