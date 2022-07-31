267e3ed45b94a5e0ab0ebe44ff278b90a1b1aa9c
Remilekun Salami <remilekun.salami@gmail.com>
2022-07-31 23:59:12 +0100

    fix: update schema

--- src/api/preview/controllers/preview.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  preview controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::preview.preview');
--- src/api/preview/services/preview.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * preview service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::preview.preview');
